86ece5405737a8e7f8d113baddcfb262bac23953
macosta2015 <macosta2015@my.fit.edu>
2024-08-05 22:54:22 -0400

    Update selectOkButton.js

--- components/selectOkButton.js
+++ components/selectOkButton.js
@@ -16,19 +16,3 @@ async function selectOkButton(newPage) {
 
 module.exports = selectOkButton;
 
-// async function selectOkButton(newPage) {
-//     console.log("selectOkButton");
-
-//     console.log('SELECTING THE OK ITEM ON THE LIST.');
-//     await newPage.evaluate(() => {
-//         const okButtonDiv = document.querySelector('div.ns-dialog-button-ok.button-ok');
-//         if (okButtonDiv) {
-//             okButtonDiv.click();
-//             okButtonDiv.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
-//         } else {
-//             console.error('OK button not found.');
-//         }
-//     });
-// }
-
-// module.exports = selectOkButton;
